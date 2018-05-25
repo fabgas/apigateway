48e74b6658e901b25c9a9d8d3f995a1f9024ba9e
fabrice.gastineau <fabrice.gastineau@gmail.com>
2018-05-25 19:56:36 +0200

    Customer Verticle

---
+++ apigateway/src/main/java/org/alma/apigateway/ApiGatewayVerticle.java
@@ -0,0 +1,13 @@
+package org.alma.apigateway;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class ApiGatewayVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		super.start(startFuture);
+	}
+
+}
---
+++ customer/src/test/java/org/alma/apigateway/CustomerRestVerticleTest.java
@@ -0,0 +1,44 @@
+package org.alma.apigateway;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class  CustomerRestVerticleTest {
+
+  private Vertx vertx;
+
+  @Before
+  public void setUp(TestContext context) {
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(CustomerRestVerticle.class.getName(),
+        context.asyncAssertSuccess());
+  }
+
+  @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testMyApplication(TestContext context) {
+    final Async async = context.async();
+
+    vertx.createHttpClient().getNow(7000, "localhost", "/api/order",
+     response -> {
+      response.handler(body -> {
+        context.assertTrue(body.toString().contains("Hello"));
+        async.complete();
+      });
+    });
+  }
+}
+
